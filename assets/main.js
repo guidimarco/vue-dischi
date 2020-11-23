ab1f6a01af05b27aa49d1a375fd997c5324e61ee
Marco Guidi <mark.88g@gmail.com>
2020-11-23 15:09:52 +0100

    HTML-CSS inserito select JS creato array con generi

--- assets/main.js
+++ assets/main.js
@@ -2,17 +2,35 @@ var app = new Vue({ // VUE INSTANCE
     el: "#wrapper",
     data: {
         currentDiscIndex: 0,
+        discGenre: [],
         discs: [],
     },
     methods: {
+        getDiscs: function() {
+            axios // GET discs from API boolean
+                .get("https://flynn.boolean.careers/exercises/api/array/music")
+                .then( (discsObj) => {
+                    // copy the discs-array into this discs (LOCAL)
+                    this.discs = discsObj.data.response;
 
+                    // get GENRE for each disc and save into this discGenre
+                    this.getDiscGenre();
+                })
+            ;
+        },
+        getDiscGenre: function() {
+            this.discs.forEach( (disc) => {
+                let currentGenre = disc.genre;
+
+                if (!this.discGenre.includes(currentGenre)) {
+                    // current genre it's not includes --> push
+                    this.discGenre.push(currentGenre);
+                }
+
+            });
+        },
     },
     mounted() {
-        axios // GET discs from API boolean
-            .get("https://flynn.boolean.careers/exercises/api/array/music")
-            .then( (discsObj) => {
-                this.discs = discsObj.data.response;
-            })
-        ;
+        this.getDiscs();
     },
 });

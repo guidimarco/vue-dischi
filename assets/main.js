d6ca45ed36be768c2a0103327ee27fc6850aba35
Marco Guidi <mark.88g@gmail.com>
2020-11-23 14:10:05 +0100

    HTML-CSS spotify caricato JS file vuoto

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+var app = new Vue({ // VUE INSTANCE
+    el: "#wrapper",
+    data: {
+        // VAR
+
+        // OBJ
+
+        // ARRAY
+    },
+    methods: {
+
+    },
+    mounted() {
+        console.log("prova");
+    },
+});
